5a0692ea09cda9a12e2e49ba658a25716c8521d9
yang <17621615772@163.com>
2019-10-29 22:41:43 +0800

    添加二分查找

---
+++ LeetCode/src/com/ecnu/binary/DivideBinary.java
@@ -0,0 +1,33 @@
+package com.ecnu.binary;
+
+public class DivideBinary {
+
+    public static void main(String[] args) {
+        System.out.println(divide(7, -3));
+    }
+
+    public static int divide(int dividend, int divisor) {
+        boolean sign = (dividend > 0) ^ (divisor > 0);
+        int result = 0;
+        if (dividend > 0) {
+            dividend = -dividend;
+        }
+        if (divisor > 0) divisor = -divisor;
+        while (dividend <= divisor) {
+            int temp_result = -1;
+            int temp_divisor = divisor;
+            while (dividend <= (temp_divisor << 1)) {
+                if (temp_divisor <= (Integer.MIN_VALUE >> 1)) break;
+                temp_result = temp_result << 1;
+                temp_divisor = temp_divisor << 1;
+            }
+            dividend = dividend - temp_divisor;
+            result += temp_result;
+        }
+        if (!sign) {
+            if (result <= Integer.MIN_VALUE) return Integer.MAX_VALUE;
+            result = -result;
+        }
+        return result;
+    }
+}
